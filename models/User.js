6001636c4e7f39df112926482fdf69094790ca5c
Athena <athenawilbon1994@aol.com>
2023-05-10 22:01:25 -0400

    User model and comments added

---
+++ models/User.js
@@ -0,0 +1,37 @@
+// Importing dependencies from mongoose library
+const { Schema, model, Types } = require('mongoose'); 
+
+// User Schema and email validation
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },    email: {
+        type: String,
+        required: true,
+        unique: true,
+       
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+// Virtual friend count defined and returned in friends array
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// User model created
+const User = model('User', userSchema);
+
+// User model export
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// User and Thought models imported
+const User = require('./User');
+const Thought = require('./Thought');
+
+// User and Thought exported 
+module.exports= {User, Thought};
